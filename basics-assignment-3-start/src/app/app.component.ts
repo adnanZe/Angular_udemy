1ee702aa9eb14421dc573b46e5bcf70cfb070e3e
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-07-30 16:51:37 +0300

    finished assignment

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  isVisible = false;
+  clickLogs = [];
+
+  onDisplayDetails() {
+    this.isVisible = !this.isVisible;
+    this.clickLogs.push(
+      `Password is ${this.isVisible ? "visible" : "hidden"} ${Date.now()}`
+    );
+  }
 }
