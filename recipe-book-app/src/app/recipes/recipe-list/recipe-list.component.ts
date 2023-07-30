c986501461a31348d7751738a4382baa52aad1f8
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-07-30 18:25:08 +0300

    displaying recipe details

--- recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://static.toiimg.com/thumb/95140230.cms?width=800&height=533&imgsize=115036'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://static.toiimg.com/thumb/95140230.cms?width=800&height=533&imgsize=115036'
+    ),
+  ];
 
   constructor() {}
 
