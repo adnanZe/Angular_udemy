064f1f233ab63d9154f6893afb0811948ccdf9ff
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-07-31 13:16:31 +0300

    binding to custom events

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -13,4 +13,25 @@ export class AppComponent {
       content: 'Just a test!',
     },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    console.log('hit');
+
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
