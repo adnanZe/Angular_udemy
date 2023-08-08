d01cb061033b97d223eba7edc6d647f4f97d006c
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-08-08 10:08:04 +0300

    small renaming

--- recipe-book-app-update/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book-app-update/src/app/shopping-list/shopping-list.component.ts
@@ -11,13 +11,13 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  private igChangedSub: Subscription;
+  private igChangeSub: Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.igChangedSub = this.slService.ingredientsChanged.subscribe(
+    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
@@ -25,6 +25,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.igChangedSub.unsubscribe();
+    this.igChangeSub.unsubscribe();
   }
 }
