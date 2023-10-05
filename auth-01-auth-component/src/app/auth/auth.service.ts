40a20aef0962934bccd5452ffcc1d18881adf078
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-10-05 20:19:07 +0300

    adding auto login

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -80,6 +80,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem("userData", JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -99,6 +100,29 @@ export class AuthService {
         break;
     }
     return throwError(errorMessage);
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationData: string;
+    } = JSON.parse(localStorage.getItem("userData"));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationData)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   logout() {
