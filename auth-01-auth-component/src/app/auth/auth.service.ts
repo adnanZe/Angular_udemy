fd9bafaa8a5a4499dcdc984f07dbf3d580c9be28
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-10-05 19:26:03 +0300

    adding logout

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -3,76 +3,106 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData {
-    kind: string;
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-    registered?: boolean;
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
-    providedIn: "root",
+  providedIn: "root",
 })
 export class AuthService {
-    user = new BehaviorSubject<User>(null);
+  user = new BehaviorSubject<User>(null);
 
-    private apiKey: string = process.env.FIREBASE_KEY;
+  private apiKey: string = process.env.FIREBASE_KEY;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-    singUp(email: string, password: string) {
-        return this.http
-            .post<AuthResponseData>(
-                `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apiKey}`,
-                {
-                    email: email,
-                    password: password,
-                    returnSecureToken: true,
-                }
-            )
-            .pipe(
-                catchError(this.handleError),
-                tap((resData => this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)))
-            );
-    }
-
-    login(email: string, password: string) {
-        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apiKey}`, {
-            email: email,
-            password: password,
-            returnSecureToken: true,
-        }).pipe(
-            catchError(this.handleError),
-            tap((resData => this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)))
-        );
-    }
-
-    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
-        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-        const user = new User(email, userId, token, expirationDate);
-        this.user.next(user);
-    }
-
-    private handleError(errorRes: HttpErrorResponse) {
-        let errorMessage = "An unknown error occurred!";
-        if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
+  singUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apiKey}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
         }
-        switch (errorRes.error.error.message) {
-            case "EMAIL_EXISTS":
-                errorMessage = "This email exists already!";
-                break;
-            case "EMAIL_NOT_FOUND":
-                errorMessage = "This email does not exist!";
-                break;
-            case "INVALID_PASSWORD":
-                errorMessage = "This password is not correct!";
-                break;
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) =>
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        )
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apiKey}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
         }
-        return throwError(errorMessage);
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) =>
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        )
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occurred!";
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
     }
+    switch (errorRes.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "This email exists already!";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "This email does not exist!";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "This password is not correct!";
+        break;
+    }
+    return throwError(errorMessage);
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(["/auth"]);
+  }
 }
--- auth-01-auth-component/src/app/header/header.component.ts
+++ auth-01-auth-component/src/app/header/header.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { DataStorageService } from '../shared/data-storage.service';
-import { AuthService } from '../auth/auth.service';
-import { Subscription } from 'rxjs';
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html",
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
   isAuthenticated = false;
 
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.authService.user.subscribe((user) => {
       this.isAuthenticated = !!user;
     });
   }
@@ -30,5 +33,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
