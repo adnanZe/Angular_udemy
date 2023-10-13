dcdc3069fb0c0fe24f6b56f98916af4bffe156a5
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-10-13 07:43:50 +0300

    testing dependencies and component

--- unit-testing/src/app/user/user.component.spec.ts
+++ unit-testing/src/app/user/user.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 import { UserComponent } from "./user.component";
+import { UserService } from "./user.service";
 
 describe('UserComponent', () => {
   beforeEach(() => {
@@ -12,5 +13,30 @@ describe('UserComponent', () => {
     const fixture = TestBed.createComponent(UserComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should use the user name from the service', () => {
+    const fixture = TestBed.createComponent(UserComponent);
+    const app = fixture.componentInstance;
+    const userService = fixture.debugElement.injector.get(UserService);
+    fixture.detectChanges();
+    expect(userService.user.name).toEqual(app.user.name);
+  });
+
+  it('should display the user name if user is logged in', () => {
+    const fixture = TestBed.createComponent(UserComponent);
+    const app = fixture.componentInstance;
+    app.isLoggedIn = true;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(app.user.name);
+  });
+
+  it('shouldn\'t display the user name if user is not logged in', () => {
+    const fixture = TestBed.createComponent(UserComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
   });
 });
--- unit-testing/src/app/user/user.component.ts
+++ unit-testing/src/app/user/user.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [UserService] // This is the key to the solution
 })
-export class UserComponent {
-  user: { name: string } = { name: 'John' };
+export class UserComponent implements OnInit {
+  user: { name: string } = { name: '' };
   isLoggedIn = false;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.user = this.userService.user;
+  }
 }
