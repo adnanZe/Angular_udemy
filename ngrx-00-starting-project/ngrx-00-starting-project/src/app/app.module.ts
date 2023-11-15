49fa5f1f1edc4ca4f984082469a06c3bf7f5a85e
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-11-15 07:58:15 +0200

    setting store in module

--- ngrx-00-starting-project/ngrx-00-starting-project/src/app/app.module.ts
+++ ngrx-00-starting-project/ngrx-00-starting-project/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -14,10 +16,11 @@ import { CoreModule } from './core.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
     SharedModule,
     CoreModule,
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
 })
-export class AppModule {}
+export class AppModule { }
